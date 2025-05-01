a95210bef0bd98613876a51df659b35cbdcb02bf
carlotasantos <149483188+carlotasantos@users.noreply.github.com>
2025-05-01 16:16:34 +0100

    Recolha dos dados

--- codigo_trabalho.py
+++ codigo_trabalho.py
@@ -5,6 +5,16 @@ path_desemprego = "C:\\Users\\carlo\\Licenciatura\\2_ano\\2_semestre\\EIA\\proje
 path_envelhecimento = "C:\\Users\\carlo\\Licenciatura\\2_ano\\2_semestre\\EIA\\projeto\\envelhicimento.csv"
 path_emprego = "C:\\Users\\carlo\\Licenciatura\\2_ano\\2_semestre\\EIA\\projeto\\populacao_empregada.csv"
 
+#Ricardo
+#path_desemprego = ""
+#path_envelhecimento = ""
+#path_emprego = ""
+
+#Vasco
+#path_desemprego = ""
+#path_envelhecimento = ""
+#path_emprego = ""
+
 df_desemprego = pd.read_csv(path_desemprego, sep=';')
 df_envelhecimento = pd.read_csv(path_envelhecimento, sep=';')
 df_emprego = pd.read_csv(path_emprego, sep=';')
@@ -14,4 +24,4 @@ print("Colunas do Desemprego:", df_desemprego.columns)
 print("Colunas do Envelhecimento:", df_envelhecimento.columns)
 print("Colunas do Emprego:", df_emprego.columns)
 
-#
+
