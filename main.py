a44dc91ceb43106ada09cdce19570effe2080150
RickyFC-Git <costarf2006@gmail.com>
2025-05-23 21:29:47 +0100

    Analise de dados por acabar

--- main.py
+++ main.py
@@ -1,16 +1,11 @@
-from utils import ler_csv, preparar_dados
-from eda import mostrar_boxplots, mostrar_histogramas, mapa_corelacao
 import pandas as pd
+from eda import valores_falta, estatisticas_descritivas, correlacao, outliers
 
-#Caminhos dos ficheiros
-path_juncao = "juncao.csv"
+df = pd.read_csv(r"C:\Programming\Elementos\TPcsv\juncao_corrigida.csv", sep = ";")
+dados_numericos = df.drop(columns=['Ano', 'Território'])
 
-#Leitura e preparação dos dados
-df_desemprego = preparar_dados(ler_csv(path_juncao),"Desemprego_jovem")
-df_envelhecimento = preparar_dados(ler_csv(path_juncao), "Indice_Envelhecimento")
-df_emprego = preparar_dados(ler_csv(path_juncao), "População_Empregada")
-
-#Integração dos dados
-df_merge = pd.merge(df_envelhecimento, df_envelhecimento, df_desemprego,on =["Ano","Municipio"],how="inner")
-df_final = pd.merge(df_merge, df_emprego, on= ["Ano","Municipio"],how="inner")
+valores_falta(df)
+estatisticas_descritivas(dados_numericos)
+correlacao(dados_numericos)
+outliers(dados_numericos)
 
