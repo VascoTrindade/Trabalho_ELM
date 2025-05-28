8bacdfd0c3b57bd7a23ece6dd0285b87ec0e8cbd
RickyFC-Git <costarf2006@gmail.com>
2025-05-28 11:34:04 +0100

    algumas alterações

--- Outliners.py
+++ Outliners.py
@@ -1,18 +1,19 @@
-import pandas as pd
+#Pode ser interessante manter por causa do IQR
+# import pandas as pd
+#
+# #Pode tornar-se numa função
+# df = pd.read_csv(r'C:\Programming\Elementos\TPcsv\juncao_corrigida.csv', sep=';')
+#
+# df['Total'] = pd.to_numeric(df['Total'], errors='coerce')
+#
+# Q1 = df['Total'].quantile(0.25)
+# Q3 = df['Total'].quantile(0.75)
+# IQR = Q3 - Q1
+#
+# limite_inferior = Q1 - 1.5 * IQR
+# limite_superior = Q3 + 1.5 * IQR
 
-#Pode tornar-se numa função
-df = pd.read_csv('C:\Programming\Elementos\Trabalho\Trabalho_ELM\dados\juncao.csv', sep=';')
+#outliers = df[(df['Total'] < limite_inferior) | (df['Total'] > limite_superior)]
 
-df['09. Valor'] = pd.to_numeric(df['09. Valor'], errors='coerce')
-
-Q1 = df['09. Valor'].quantile(0.25)
-Q3 = df['09. Valor'].quantile(0.75)
-IQR = Q3 - Q1
-
-limite_inferior = Q1 - 1.5 * IQR
-limite_superior = Q3 + 1.5 * IQR
-
-outliers = df[(df['09. Valor'] < limite_inferior) | (df['09. Valor'] > limite_superior)]
-
-print(f'Total de outliers encontrados: {len(outliers)}')
-print(outliers.head())
+#print(f'Total de outliers encontrados: {len(outliers)}')
+#print(outliers.head())
