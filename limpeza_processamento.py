9ab63ec8d375fa933ecac354f4dcb5a7dcc56fa5
carlotasantos <149483188+carlotasantos@users.noreply.github.com>
2025-05-28 11:37:06 +0100

    tratamento de outliers

--- limpeza_processamento.py
+++ limpeza_processamento.py
@@ -5,3 +5,11 @@ def tratar_valores_em_falta(df, limiar_colunas=0.5):
     df = df.fillna(df.mean(numeric_only=True))
     return df
 
+def win_outliers(df):
+    for col in df.select_dtypes(include=['number']).columns:
+        Q1 = df[col].quantile(0.25)
+        Q3 = df[col].quantile(0.75)
+        IQR = Q3 - Q1
+        low, high = Q1 - 1.5 * IQR, Q3 + 1.5 * IQR
+        df[col] = df[col].clip(lower=low, upper=high)
+        return df
