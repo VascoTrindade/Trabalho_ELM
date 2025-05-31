3b679da40bbdf2360f79e24637856a9cc13d1d02
carlotasantos <149483188+carlotasantos@users.noreply.github.com>
2025-05-31 18:21:53 +0100

    alteraçao do caminho

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from eda import valores_falta, estatisticas_descritivas, correlacao, outliers
 from a_descritiva import aplicar_kmeans
 from limpeza import limpeza_dados
 
-entrada = r"C:\Programming\Elementos\Trabalho\Trabalho_ELM\juncao_corrigida.csv"
-saida = r"C:\Programming\Elementos\Trabalho\Trabalho_ELM\juncao_limpo.csv"
+entrada = "C:\\Users\\carlo\\OneDrive\\Ambiente de Trabalho\\Universidade\\Licenciatura\\2ª Ano de Licenciatura\\1º Semestre\\Programação (1ºano)\\Trabalho_ELM\\juncao_corrigida.csv"
+saida = "C:\\Users\\carlo\\OneDrive\\Ambiente de Trabalho\\Universidade\\Licenciatura\\2ª Ano de Licenciatura\\1º Semestre\\Programação (1ºano)\\Trabalho_ELM\\juncao_limpa.csv"
 
 df_inicial = pd.read_csv(entrada, sep=";")
 
